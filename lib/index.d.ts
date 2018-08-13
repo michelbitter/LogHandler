6efd725fc9b9d52e58dccf89d4e765d512d0a561
Michel Bitter <michel@michelbitter.nl>
2018-08-13 10:37:58 +0200

    new version!
    Signed-off-by: Michel Bitter <michel@michelbitter.nl>

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,9 @@
-import { Config, ReportersInterface, LogObjectInterface, LogLevels } from './interfaces';
-declare const logHandler: (config: Config) => import("./interfaces").LogHandlerResults;
+import { Config, ReportersInterface, LogObjectInterface, LogLevels, LogHandlerResults } from './interfaces';
+declare const logHandler: (config: Config) => LogHandlerResults;
 export default logHandler;
 export declare type ReportersInterface = ReportersInterface;
 export declare type Config = Config;
 export declare type LogObjectInterface = LogObjectInterface;
 export declare type LogLevels = LogLevels;
+export declare type Log = LogHandlerResults;
 export declare const logLevelsKeys: ("emerg" | "alert" | "crit" | "err" | "warning" | "notice" | "info" | "debug")[];
