21f980cbde0b3f76ea02c8fc035955b18b4a542b
Michel Bitter <michel@michelbitter.nl>
2019-08-02 00:49:22 +0200

    remove reporters from version. They come back as seperate packages

--- lib/reporters/console/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReportersInterface, LogObjectInterface } from '../..';
-export declare class ConsoleReporter implements ReportersInterface {
-    readonly name = "Console reporter";
-    readonly timeOut = 2500;
-    log(obj: LogObjectInterface): Promise<void>;
-}
